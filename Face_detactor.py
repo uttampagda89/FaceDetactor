1f4d00818cafad761b85e30cf5219db3a5d1c1a8
uttampagda <47469151+uttampagda@users.noreply.github.com>
2020-10-02 10:14:18 +0530

    Update Face_detactor.py

--- Face_detactor.py
+++ Face_detactor.py
@@ -4,7 +4,7 @@ import cv2
 
 trained_face_data=cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
-webcam=cv2.VideoCapture("madeinindia.mp4")
+webcam=cv2.VideoCapture(0)# 0 for using webcam
 
 while True:
   successful_frame_read,frame = webcam.read()
