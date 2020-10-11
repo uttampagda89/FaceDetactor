99cab0db8ba1ac44c7710dfe9f6bb7d8ab197596
uttampagda <47469151+uttampagda@users.noreply.github.com>
2020-10-11 10:09:20 +0530

    Update Face_detactor.py

--- Face_detactor.py
+++ Face_detactor.py
@@ -12,7 +12,7 @@ while True:
   face_coordinates=trained_face_data.detectMultiScale(grayscaled_img)
   for (x,y,w,h) in face_coordinates:
     cv2.rectangle(frame,(x,y),(x+w,y+h),(0,0,0),10). # Black box around face
-  cv2.imshow("Uttam Face Detactor",frame)
+  cv2.imshow("Face Detactor",frame)
   key=cv2.waitKey(1)
 
   if key==81 or key==113:
